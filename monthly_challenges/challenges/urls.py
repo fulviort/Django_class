8c57e4472e2c02eecbba2767f47f5676953c8535
Fulvio Rodriguez Casa <fulviort@hotmail.com>
2023-09-05 09:54:43 -0400

    avances del dia

--- monthly_challenges/challenges/urls.py
+++ monthly_challenges/challenges/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path
 from . import views
 
+# we have different ways of connecting urls, dynamic way with a main name, or 1 by 1.
+
 urlpatterns = [
-    path("january", views.index)
+    path("test1", views.test1),
+    path("test2", views.test2),
+    path("test3", views.test3),
+    path("<int:month>", views.monthly_challenges_by_number),
+    path("<str:month>", views.monthly_challenge)
 ]
--- monthly_challenges/challenges/views.py
+++ monthly_challenges/challenges/views.py
@@ -1,8 +1,52 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseNotFound
+
+monthly_challenges = {
+    "january:": "This works in january, eat fruits!",
+    "february": "This works in february, study atleast 20 minutes",
+    "march": "This works in march, eat veggies",
+    "april": "This works in march, eat veggies",
+    "may": "This works in march, eat veggies5",
+    "june": "This works in march, eat veggies6",
+    "july": "This works in march, eat veggies7",
+    "august": "This works in march, eat veggies8",
+    "september": "This works in march, eat veggies9",
+    "october": "This works in march, eat veggies10",
+    "november": "This works in march, eat veggies11",
+    "december": "This works in march, eat veggies12"
+}
 
 # Create your views here.
 
 
-def index(request):
-    return HttpResponse("This works!")
+def test1(request):
+    return HttpResponse("test1")
+
+
+def test2(request):
+    return HttpResponse("test2")
+
+
+def test3(request):
+    return HttpResponse("test3")
+
+
+def monthly_challenges_by_number(request, month):
+    return HttpResponse(month)
+
+def monthly_challenge(request, month):
+    try:
+        challenge_text = monthly_challenges[month]
+        return HttpResponse(challenge_text)
+    except:
+        return HttpResponseNotFound("This month is not supported!")
+
+    """if month == 'january':
+        return HttpResponse("This works in january, eat fruits!")
+    elif month == 'february':
+        return HttpResponse("This works in february, study atleast 20 minutes")
+    elif month == 'march':
+        return HttpResponse("This works in march, eat veggies")
+    else:
+        return HttpResponseNotFound("this month is not registered")"""
+    

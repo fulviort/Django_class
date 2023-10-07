5c6a73369b70f83d6b565f72398bd233cd759ab1
Fulvio Laptop <fulviort@hotmail.com>
2023-10-07 09:58:38 -0400

    updates of the day, links and some css/html connecting

--- blog/views.py
+++ blog/views.py
@@ -8,7 +8,7 @@ def starting_page(request):
     return render(request, "blog/index.html")
 
 def posts(request):
-    return HttpResponse("Es la pagina de los posts")
+    return render(request, "blog/all-posts.html")
 
 def detailed_post(request):
     return HttpResponse("una sola publicacion aparte")
